142160fd8f598872f5a96f1bb89a2263d250c693
evilaaron11 <aaron.m.a@comcast.net>
2016-06-27 15:36:25 -0400

    Almost finished, need to test

--- wpaper.py
+++ wpaper.py
@@ -2,13 +2,14 @@ from ctypes import *
 from os import path
 import feedparser
 from datetime import datetime
-import urllib
+import urllib      
+import win32api
 
 SPI_SETDESKWALLPAPER = 0x14
 SPIF_UPDATEINIFILE   = 0x1
+HTML_EXT_LEN = 5
 
-#SystemParametersInfo = windll.user32.SystemParametersInfoA
-#SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, "C:\\Users\\Public\\Pictures\\background1024x768.bmp", SPIF_UPDATEINIFILE);
+SystemParametersInfo = windll.user32.SystemParametersInfoA
 
 fd = feedparser.parse("http://wallpaperswide.com/rss/index.html")
 #print fd.feed.title
@@ -24,8 +25,12 @@ print "now after they are sorted"
 #for i in toSort:
 #   print i.published[:25]
 background_url = toSort[0].link
-background_url.split(".")
+#background_url = background_url.split(".")
 
-for i in background_url:
-   print i
-#urllib.urlretrieve(i
+print background_url[:len(background_url) - HTML_EXT_LEN]
+#urllib.urlretrieve(
+monitorWxH = str(windll.user32.GetSystemMetrics(0)) + "x" + str(windll.rser32.GetSystemMetrics(1))
+saveLocation = "C:\\Users\\" + win32api.GetUserName() + "\\AppData\\Local\\Temp\\wp.jpg"
+urllib.urlretrieve(background_url + monitorWxH + ".jpg", saveLocation)
+
+SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, "C:\\Users\\Public\\Pictures\\background1024x768.bmp", SPIF_UPDATEINIFILE);
