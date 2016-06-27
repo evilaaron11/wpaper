6080bff7ac5ff93d90b7da3519246741c7cdbfa6
evilaaron11 <aaron.m.a@comcast.net>
2016-06-27 15:59:16 -0400

    Fixed bugs, seems to be working

--- wpaper.py
+++ wpaper.py
@@ -7,30 +7,25 @@ import win32api
 
 SPI_SETDESKWALLPAPER = 0x14
 SPIF_UPDATEINIFILE   = 0x1
-HTML_EXT_LEN = 5
+HTML_EXT_LEN = 6
+SITE_URL = "http://wallpaperswide.com/"
 
 SystemParametersInfo = windll.user32.SystemParametersInfoA
 
 fd = feedparser.parse("http://wallpaperswide.com/rss/index.html")
-#print fd.feed.title
-#print fd.entries[0].title
-#print fd.entries[0].link
-#print fd.entries[0].published
 
 toSort = fd.entries
-
 toSort.sort(key=lambda i: datetime.strptime(i.published[:25], "%a, %d %b %Y %H:%M:%S"), reverse = True)
-print "now after they are sorted"
 
-#for i in toSort:
-#   print i.published[:25]
 background_url = toSort[0].link
-#background_url = background_url.split(".")
+background_url= background_url[len(SITE_URL):len(background_url) - HTML_EXT_LEN] + "-"
 
-print background_url[:len(background_url) - HTML_EXT_LEN]
-#urllib.urlretrieve(
-monitorWxH = str(windll.user32.GetSystemMetrics(0)) + "x" + str(windll.rser32.GetSystemMetrics(1))
+monitorWxH = str(windll.user32.GetSystemMetrics(0)) + "x" + str(windll.user32.GetSystemMetrics(1))
 saveLocation = "C:\\Users\\" + win32api.GetUserName() + "\\AppData\\Local\\Temp\\wp.jpg"
-urllib.urlretrieve(background_url + monitorWxH + ".jpg", saveLocation)
 
-SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, "C:\\Users\\Public\\Pictures\\background1024x768.bmp", SPIF_UPDATEINIFILE);
+#print "Downloading wallpaper..."
+
+urllib.urlretrieve(SITE_URL + "/download/" + background_url + monitorWxH + ".jpg", saveLocation)
+
+#print "Wallpaper set to " + toSort[0].title
+SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, saveLocation, SPIF_UPDATEINIFILE);
